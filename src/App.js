641b33de37813d13f520bb7d174372b032473d2c
Mats Roshauw <karmats@gmail.com>
2018-02-01 07:34:02 +0100

    Working prototype

--- src/App.js
+++ src/App.js
@@ -3,15 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    year: '',
+    month: '',
+    day: ''
+  }
+
+  yearChanged = (evt) => {
+    this.setState({
+      year: evt.currentTarget.value
+    })
+  }
+
+  monthChanged = (evt) => {
+    this.setState({
+      month: evt.currentTarget.value
+    })
+  }
+
+  dayChanged = (evt) => {
+    this.setState({
+      day: evt.currentTarget.value
+    })
+  }
+
+  next = () => {
+    // Create UTC date. Month - 1 because javsascript months starts with 0
+    const dob = new Date(Date.UTC(parseInt(this.state.year), parseInt(this.state.month) - 1, parseInt(this.state.day)))
+    alert(dob.toISOString().slice(0, 10))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome!</h1>
+          <h3 className="App-subtitle">To continue you need to verify that you are over 18.</h3>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input id="year" onChange={this.yearChanged} placeholder="YYYY" />
+          <input id="month" onChange={this.monthChanged} placeholder="MM" />
+          <input id="day" onChange={this.dayChanged} placeholder="DD" />
+        </p>
+        <p>
+          <button onClick={this.next}>Next</button>
         </p>
       </div>
     );
