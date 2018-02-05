60691d43038241c1c8fcef8f3e7cdc54b06816df
Mats Roshauw <karmats@gmail.com>
2018-02-05 18:59:20 +0100

    Utlity for validation

---
+++ src/util.js
@@ -0,0 +1,93 @@
+function validateNumber(numString) {
+    const num = parseInt(numString, 10);
+    if (isNaN(num)) {
+        throw new TypeError(`'${numString}' is not a number`)
+    }
+    return num;
+}
+
+/**
+ * Sanity check for year input
+ * 
+ * @param   {string} yearString Year input as string
+ * @returns {number} Year as number
+ * @throws  {TypeError} If yearString cannot be cast to number
+ * @throws  {RangeError} If year is not valid
+ */
+function validateYear(yearString) {
+    const year = validateNumber(yearString);
+    const thisYear = new Date().getUTCFullYear();
+    let error = '';
+    // Pretty unlikely that someone over 120 years wants to enter the site
+    if (year < 1900) {
+        error = `Are you really ${thisYear - year} years old?`
+    } else if (year >= thisYear) {
+        error = 'You are from the future?'
+    }
+    if (error.length) {
+        throw new RangeError(error);
+    }
+    return year;
+}
+
+/**
+ * Sanity check for month input
+ * 
+ * @param   {string} monthString Month input as string
+ * @returns {number} Month as number
+ * @throws  {TypeError} If monthString cannot be cast to number
+ * @throws  {RangeError} If month is not valid
+ */
+function validateMonth(monthString) {
+    const month = validateNumber(monthString);
+    let error = '';
+    if (month < 1 || month > 12) {
+        error = 'Month must be between 1 - 12';
+    }
+    if (error.length) {
+        throw new RangeError(error);
+    }
+    return month;
+}
+
+/**
+ * Sanity check for day input
+ * 
+ * @param   {string} dayString Day input as string
+ * @returns {number} Day as number
+ * @throws  {TypeError} If dayString cannot be cast to number
+ * @throws  {RangeError} If day is not valid
+ */
+function validateDay(dayString) {
+    const day = validateNumber(dayString);
+    let error = '';
+    if (day < 1 || day > 31) {
+        error = 'Day must be between 1 - 31';
+    }
+    if (error.length) {
+        throw new RangeError(error);
+    }
+    return day;
+}
+
+/**
+ * @param {Date} dob Date of birth
+ * @return {number} The age based on date of birth
+ */
+function age(dob) {
+    const now = new Date();
+    let age = now.getUTCFullYear() - dob.getUTCFullYear()
+    // If birthday hasn't been in this year yet, substract one year
+    if ((dob.getUTCMonth() > now.getUTCMonth()) ||
+        (dob.getUTCMonth() === now.getUTCMonth() && dob.getUTCDate() > now.getUTCDate())) {
+        age--;
+    }
+    return age;
+}
+
+export {
+    validateYear,
+    validateMonth,
+    validateDay,
+    age
+}
---
+++ src/util.test.js
@@ -0,0 +1,57 @@
+import * as util from './util'
+
+describe('util test', () => {
+
+    it('should return year as number', () => {
+        expect(util.validateYear('1981')).toBe(1981)
+    })
+
+    it('should throw error when input is not a number', () => {
+        expect(() => util.validateYear('hej')).toThrow(TypeError)
+    })
+
+    it('should throw error when year is before 1900', () => {
+        expect(() => util.validateYear('1899')).toThrow(RangeError)
+    })
+
+    it('should throw error when year is after this year', () => {
+        expect(() => util.validateYear(new Date().getFullYear().toString())).toThrow(RangeError)
+    })
+
+    it('should return month as number', () => {
+        expect(util.validateMonth('01')).toBe(1)
+    })
+
+    it('should throw error when month is invalid', () => {
+        expect(() => util.validateMonth('00')).toThrow(RangeError)
+        expect(() => util.validateMonth('13')).toThrow(RangeError)
+    })
+
+    it('should return day as number', () => {
+        expect(util.validateDay('07')).toBe(7)
+    })
+
+    it('should throw error when day is invalid', () => {
+        expect(() => util.validateMonth('00')).toThrow(RangeError)
+        expect(() => util.validateMonth('32')).toThrow(RangeError)
+    })
+
+    it('should be age of 18', () => {
+        const now = new Date();
+        const dob = new Date(now.getUTCFullYear() - 18, 0, 1)
+        expect(util.age(dob)).toBe(18);
+    })
+
+    it('should be age of 17 when birthday is later this year', () => {
+        const now = new Date();
+        const dob = new Date(now.getUTCFullYear() - 18, now.getUTCMonth() + 5, 1)
+        expect(util.age(dob)).toBe(17);
+    })
+
+    it('should be age of 17 when birthday is later this month', () => {
+        const now = new Date();
+        const dob = new Date(now.getUTCFullYear() - 18, now.getUTCMonth(), now.getUTCDate() + 5)
+        expect(util.age(dob)).toBe(17);
+    })
+
+})
